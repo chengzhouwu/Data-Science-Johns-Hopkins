edf16b1ba1f9b0262f81afd37b4005c919ac002a
ChengzhouWu <71668362+chengzhouwu@users.noreply.github.com>
2022-10-27 21:14:55 -0500

    new

---
+++ Exploratory Data Analysis/week4/plot4.R
@@ -0,0 +1,14 @@
+# Subset coal combustion related NEI data
+combustionRelated <- grepl("comb", SCC[, SCC.Level.One], ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC[, SCC.Level.Four], ignore.case=TRUE) 
+combustionSCC <- SCC[combustionRelated & coalRelated, SCC]
+combustionNEI <- NEI[NEI[,SCC] %in% combustionSCC]
+
+png("plot4.png")
+
+ggplot(combustionNEI,aes(x = factor(year),y = Emissions/10^5)) +
+  geom_bar(stat="identity", fill ="#FF9999", width=0.75) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+
+dev.off()
